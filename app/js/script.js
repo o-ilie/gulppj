a7e8764bce11c4b983d39c9d66bdfcb869c983c1
o-ilie <ilie.oana27@gmail.com>
2021-07-11 20:26:38 +0300

    resume

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,12 @@
-console.log("Load script.js");
-
-// Instantiating the global app object
-var app = {};
+$(document).ready(function () {
+  $(".nav ul li:first").addClass("active");
+  $(".sidebar-content:not(:first)").hide();
+  $(".sidebar ul li a").click(function (event) {
+    event.preventDefault();
+    var content = $(this).attr("href");
+    $(this).parent().addClass("active");
+    $(this).parent().siblings().removeClass("active");
+    $(content).show();
+    $(content).siblings(".sidebar-content").hide();
+  });
+});
