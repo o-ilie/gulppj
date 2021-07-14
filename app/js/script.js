89edeb00e7f35f1a71082d5f5524c041344f07b5
Ioana Ilie (PDP) <ioana.ilie@IDGROUP.local>
2021-07-14 09:47:31 +0300

    test

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-  $(".nav ul li:first").addClass("active");
-  $(".sidebar-content:not(:first)").hide();
-  $(".sidebar ul li a").click(function (event) {
-    event.preventDefault();
-    var content = $(this).attr("href");
-    $(this).parent().addClass("active");
-    $(this).parent().siblings().removeClass("active");
-    $(content).show();
-    $(content).siblings(".sidebar-content").hide();
+  $("ul.tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
+
+    $("ul.tabs li").removeClass("active");
+    $(".sidebar-content").removeClass("active");
+
+    $(this).addClass("active");
+    $("#" + tab_id).addClass("active");
   });
 });
